6eea983a1ccb0cf8fd1c4319141c00e5b5230bc0
Ethan Laptop Ubuntu <ethan.walker830@gmail.com>
2019-05-23 14:34:31 -0600

    new major test cases

--- code/workbook.py
+++ code/workbook.py
@@ -9,39 +9,31 @@ import autograd.numpy as anp
 
 if __name__ == "__main__":
     reload(s)
-    # A = np.array([
-    #     [0,0,0,0,0,0,1],
-    #     [1,0,1,1,0,0,0],
-    #     [0,1,0,0,0,0,0],
-    #     [0,0,1,0,0,0,0],
-    #     [0,0,1,0,0,0,0],
-    #     [0,0,0,1,1,0,1],
-    #     [0,0,0,1,0,1,0]
-    # ])
-    # labels = ["a", "b", "c", 'd','e','f','g']
 
     def sig(x):
-        # print(x)
         return anp.tanh(x)
     def sig2(x):
         y = -2.0*anp.tanh(x)
         return y
     def zero(x):
         return 0*x
+    def chaotic_func1(x):
+        y = anp.tanh(4*x*(1-x)) + 1.75
+        return y
+    def chaotic_func2(x):
+        y = anp.tanh(4*x*(1-x)) + 1.25
+        return y
+    def chaotic_func3(x):
+        y = anp.tanh(4*x*(1-x)) + 0.25
+        return y
     def func1(x):
-        # print(x)
-        y = 9/10*x + 7/4
-        # print(y)
+        y = (9/10)*x + 1.75
         return y
     def func2(x):
-        # print(x)
-        y = 9/10 * x + 5/4
-        # print(y)
+        y = (9/10)*x + 1.25
         return y
     def func3(x):
-        # print(x)
-        y = 9/10 * x + 2/4
-        # print(y)
+        y = (9/10)*x + 0.5
         return y
 
     # basic cohen-grossberg neural network
@@ -55,42 +47,14 @@ if __name__ == "__main__":
         [ sig, zero, zero],
         [ sig, sig, zero]
     ])
-    a = np.array([func1, func2, func3])
+    a = np.array([chaotic_func1, chaotic_func2, chaotic_func3])
     labels = ['x1', 'x2', 'x3']
 
-    # after the first specialization
-    # C = np.array([[0,0,0,1],[0,0,0,1],[0,1,0,0],[1,0,1,0]])
-    # labels2 = ['y1', 'y2', 'y3', 'y4']
-    # b = np.array([func1, func1, func2, func3])
-    # g = np.array([[zero, zero, zero, sig2],
-    #                 [zero, zero, zero, sig2],
-    #                 [zero, sig, zero, zero],
-    #                 [sig, zero, sig, zero]])
-
-
-    # H = s.DirectedGraph(C, (b,g), labels=labels2)
-    # H.iterate(80, np.random.random(H.n), graph=True, save_img=True, title='first spec hard code corrected')
-
-
     G = s.DirectedGraph(B, (a,f), labels=labels)
-    print(G.eigen_centrality(), '\n')
-    print(G.detect_sync(), '\n')
-    # G.iterate_with_perterbations(1000, np.random.random(G.n), ([500], 150), graph=True, save_img=False, title="3 nodes")
 
-    G.specialize(['x1', 'x2'], verbose=False)
-    # G.iterate_with_perterbations(1000, np.random.random(G.n), ([500], 150), graph=True, save_img=False, title="3 nodes")
-    G.iterate(80, np.random.random(G.n), graph=True, save_img=True, title='../graphs/spec on x1, x2')
-    print(G.eigen_centrality(), '\n')
-    print(G.detect_sync(), '\n')
-
-    G.specialize(['x2', 'x3.1', 'x3.2'], verbose=False)
-    # G.iterate_with_perterbations(1000, np.random.random(G.n), ([500], 150), graph=True, save_img=False, title="3 nodes")
-    G.iterate(80, np.random.random(G.n)*10, graph=True, save_img=True, title='../graphs/spec on x2, x31, x32')
-    print(G.eigen_centrality(), '\n')
-    print(G.detect_sync(), '\n')
-
-    # N = nx.DiGraph(G.A.T)
-    # nx.draw_networkx_labels(N, pos=nx.spring_layout(N), labels=G.labeler)
-    # nx.draw(N, pos=nx.spring_layout(N))
-    # plt.draw()
-    # plt.show()
+    for i in range(3):
+        base = np.random.choice(G.indices, 2, replace=False)
+        G.specialize(base)
+    print(G.n)
+    G.iterate_with_perturbations(300, np.random.random(G.n), ([150], 10), graph=True, save_img=True, title='../graphs/chaotic cgnn with perturbation')
+    print(G.spectral_radius())
