74b5a17c02043792e1512705c15fc04ed37a3b7a
Ethan Walker <ethan.walker830@gmail.com>
2019-09-17 19:11:41 -0600

    changed test cases

--- code/equit.py
+++ code/equit.py
@@ -10,14 +10,14 @@ import autograd.numpy as anp
 if __name__ == "__main__":
     reload(s)
     A = np.array([
-        [0,1,1,0,0,0,0,0],
-        [1,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,1,0,0,0,0,0],
         [1,1,0,1,0,0,0,0],
-        [0,0,1,0,1,0,0,0],
-        [0,0,0,1,0,1,0,0],
-        [0,0,0,0,1,0,1,1],
-        [0,0,0,0,0,1,0,1],
-        [0,0,0,0,0,1,1,0]
+        [1,0,1,0,0,0,0,0],
+        [1,0,0,0,0,1,0,0],
+        [1,0,0,0,1,0,1,0],
+        [0,0,0,0,0,1,0,0],
+        [0,1,1,1,1,1,1,0]
     ])
 
     def sig(x):
@@ -29,28 +29,32 @@ if __name__ == "__main__":
     def zero(x):
         return 0*x
     def f1(x):
-        return 0.9*x + 1/5
+        return 0.5*x + 1/5
     def f2(x):
         return 0.9*x + 7/5
     def f3(x):
         return 0.9*x + 0.5
 
     f = np.array([
-        [zero,zero,zero,zero,zero,zero,zero,zero],
-        [zero,zero,zero,zero,zero,zero,zero,zero],
-        [zero,zero,zero,zero,zero,zero,zero,zero],
-        [zero,zero,zero,zero,zero,zero,zero,zero],
-        [zero,zero,zero,zero,zero,zero,zero,zero],
-        [zero,zero,zero,zero,zero,zero,zero,zero],
-        [zero,zero,zero,zero,zero,zero,zero,zero]
+        [f1,f1,f1,f1,f1,f1,f1,f1],
+        [f1,f1,f1,f1,f1,f1,f1,f1],
+        [f1,f1,f1,f1,f1,f1,f1,f1],
+        [f1,f1,f1,f1,f1,f1,f1,f1],
+        [f1,f1,f1,f1,f1,f1,f1,f1],
+        [f1,f1,f1,f1,f1,f1,f1,f1],
+        [f1,f1,f1,f1,f1,f1,f1,f1],
+        [f1,f1,f1,f1,f1,f1,f1,f1]
     ])
 
-    a = np.array([zero,zero,zero,zero,zero,zero,zero,zero,zero,zero])
-    labels = ['0','1','2','3','4','5','6','7']
+    a = np.array([sig,sig,sig,sig,sig,sig,sig,sig])
+    labels = ['1','2','3','4','5','6','7','8']
 
     G = s.DirectedGraph(A, (a,f), labels=labels)
     # print(G.coloring())
-    G.network_vis(use_eqp=True)
-    base = ['2','3','4','5','6','7']
+    # G.network_vis()#use_eqp=True)
+    G.iterate(300,np.random.random(8),graph=True)
+    base = ['1','8']
     G.specialize(base)
-    G.network_vis(use_eqp=True)
+    G.iterate(300,np.random.random(38),graph=True)
+    # G.network_vis()#use_eqp=True)
+    # print(G.n)
